1f44324084965348188d1262a2f9f0d8ae5955a6
leidypiramanrique <leidy.piramanrique@gmail.com>
2026-05-25 17:56:56 -0500

    Update defaults.js
    cambio precio a cop

--- server/config/defaults.js
+++ server/config/defaults.js
@@ -2,14 +2,16 @@
  * Default configuration values for NetPolix
  */
 module.exports = {
-  // Pricing defaults
+  // Pricing defaults en pesos colombianos (COP)
   pricing: {
-    movieRentalPrice: 4.99,
-    movieSalePrice: 14.99,
-    seriesRentalPrice: 7.99,
-    seriesSalePrice: 24.99,
-    collectionRentalPrice: 12.99,
-    collectionSalePrice: 39.99
+    movieRentalPrice: 15000,      // COP
+    movieSalePrice: 45000,        // COP
+
+    seriesRentalPrice: 25000,     // COP
+    seriesSalePrice: 70000,       // COP
+
+    collectionRentalPrice: 40000, // COP
+    collectionSalePrice: 120000   // COP
   },
   
   // Points system
@@ -17,8 +19,11 @@ module.exports = {
     pointsPerRental: 10,
     pointsPerSale: 25,
     pointsPerReferral: 50,
-    pointsRedemptionRate: 100, // 100 points = $1 discount
+    pointsRedemptionRate: 1000 // 1000 puntos = 1000 COP
   },
+
+  // Currency
+  currency: 'COP',
 
   // Valid classifications
   classifications: ['G', 'PG', 'PG-13', 'R', 'NC-17'],
